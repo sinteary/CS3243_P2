f1b4114f07c66cf0ba64ff70a7a70fab256f38a4
Derek-Hardy <derekzxj@outlook.com>
2020-03-29 01:15:53 +0800

    Added forward checking and heuristic to sort by degree of constraints; Updated the results in readme

--- README.md
+++ README.md
@@ -4,13 +4,20 @@
 
 Navigate to the folder `CS3243_P2/sudoku`, run the command `python CS3243_P2_Sudoku_47.py input4.txt dump.txt` to test the solver algorithm.
 
+The performance time below includes the printing of debugging table. Actual performance is faster when no debug information is printed on the console.
+
+
+**General algorithm**
+* Maintain the list of assignable numbers for each empty cell on the board as constraints
+* Use backtracking to try all the assignable numbers for each cell 
+  * If the trial does not violate Sudoku rules, proceed to the next empty cell.
+  * If it violates, backtrack by resetting the current cell empty and go back to the previous assignment.
+* After each valid assignment, update the list of assignable numbers for all remaining empty cells on the board
+
 
 **Approach 1:**
-* Maintain the list of assignable numbers for each empty cell on the board as constraints.
-* Use backtracking to try all the assignable numbers for each cell in ascending order.
-	* If succeed, proceed to the next empty cell.
-	* If failed, backtrack by resetting the current cell empty and go back to the previous assignment.
-* After each valid assignment, update the constraints for all remaining empty cells on the board
+* Fill the empty cell from top-down, left to right
+* Try out each assignable number in ascending order
 
 **Results:**
 * Input1: `24.9235s`
@@ -20,18 +27,53 @@ Navigate to the folder `CS3243_P2/sudoku`, run the command `python CS3243_P2_Sud
 
 
 **Approach 2:**
-* Same as the approach 1, except the assignment of valid candidates is in random order
+* Fill the empty cell from top-down, left to right
+* Try out each assignable number in random order
 
 **Results:**
 
 * Input1: `25.6315s (better case),  213.7323s (worse case)`
-* Input2: `6.1811s  (better cases), 13.1004s  (worse case)`
-* Input3: `1.8654s`
-* Input4: `0.3256s`
+* Input2: `6.1811s  (better cases), 12.1366s  (worse case)`
+* Input3: `0.9792s`
+* Input4: `0.7423s`
 
 > Notes:
 
 > The time taken varies largely. On average, there is no significant improvements from approach 1. For more difficult cases like input1 and input2, this approach struggles to compute the answer occasionally.
+
+
+**Approach 3:**
+* Fill the empty cell from top-down, left to right
+* Try out each assignable number in ascending order
+* Perform forward checking: stop early when constraints are 
+not satisfiable (some empty cells already have no assignable number)
+
+* Input1: `11.5302s`
+* Input2: `0.6481s`
+* Input3: `0.3025s`
+* Input4: `0.2247s`
+
+> Notes:
+
+> The addition of forward checking has improved performance
+by more than half in most cases.
+
+
+**Approach 4:**
+* Fill the empty cell starting with the highest degree of constraints (least number of assignable number)
+* Try out each assignable number in ascending order
+* Perform forward checking: stop early when constraints are 
+not satisfiable (some empty cells already have no assignable number)
+
+
+* Input1: `3.9446s`
+* Input2: `0.04649s`
+* Input3: `0.01629s`
+* Input4: `0.02353s`
+
+> Notes:
+
+> The heuristic of "sorting by degree of constraints" has further improved the performance. It is because we further narrowed down the number of possibilities to try the assignment.
 
 
 ## Task 2: The Pacman Game
--- sudoku/CS3243_P2_Sudoku_47.py
+++ sudoku/CS3243_P2_Sudoku_47.py
@@ -2,6 +2,7 @@ import sys
 import copy
 import time
 import random
+import heapq as pq
 
 # Running script: given code can be run with the command:
 # python file.py, ./path/to/init_state.txt ./output/output.txt
@@ -22,7 +23,9 @@ class Sudoku(object):
         # Call the the Sudoku solver method
         start = time.time()
         # self.csp_1()
-        self.csp_2()
+        # self.csp_2()
+        # self.csp_3()
+        self.csp_4()
         end = time.time()
 
         print("Time taken to finish: " + str(end - start))
@@ -39,8 +42,9 @@ class Sudoku(object):
 
     def csp1_helper(self):
         """
-        Perform backtracking to fill each empty cell with value that satisfies the 
-        constraints of Sudoku.
+        Approach 1:
+        	- Fill the empty cell row by row
+        	- Try out the valid candidates in ascending order
         """
         for x in range(len(self.ans)):
             for y in range(len(self.ans[0])):
@@ -56,7 +60,10 @@ class Sudoku(object):
                         # Among all valid candidates for the current empty cell, try the value c
                         self.ans[x][y] = c
 
+                        # FOR DEBUGGING ONLY
+                        # SHOULD BE REMOVED IN SUBMISSION!
                         self.print_table()
+
                         # Keep a copy of the old constraints
                         old_constraints = copy.deepcopy(self.constraints)
                         # Update the constraints with newly assigned value c
@@ -82,62 +89,239 @@ class Sudoku(object):
 
 
     def csp_2(self):
-        # Initialise the constraints on the initial given Sudoku board
         self.constraints = self.get_constraints();
-        # Call the helper function to solve Sudoku by performing backtracking on the constraints
         self.csp2_helper()
         
 
     def csp2_helper(self):
         """
-        Perform backtracking to fill each empty cell with value that satisfies the 
-        constraints of Sudoku.
+        Approach 2:
+        	- Fill the empty cell row by row
+        	- Try out the valid candidates in random order
         """
         for x in range(len(self.ans)):
             for y in range(len(self.ans[0])):
-                # When we find an empty cell
                 if (self.ans[x][y] == 0):
-                    # Use the coordinates of the current empty cell to form the key
-                    # The key is used to get the corresponding list of candidate values
                     location = (x, y)
-                    # Get a list of valid candidate values for the current empty cell
                     candidates = self.constraints.get(location)
                     # Try out the candidate in random order
                     random.shuffle(candidates)
 
                     for c in candidates:
-                        # Among all valid candidates for the current empty cell, try the value c
                         self.ans[x][y] = c
 
+                        # FOR DEBUGGING ONLY
+                        # SHOULD BE REMOVED IN SUBMISSION!
                         self.print_table()
-                        # Keep a copy of the old constraints
+
                         old_constraints = copy.deepcopy(self.constraints)
-                        # Update the constraints with newly assigned value c
                         self.constraints = self.get_constraints()
 
-                        # Proceed to the next empty cell by filling the current empty cell with value c
-                        if (self.csp1_helper()):
+                        if (self.csp2_helper()):
                             return True;
 
-                        # The value c is not a valid trial, backtrack by resetting this cell to empty again
-                        # in order to try other values
                         self.ans[x][y] = 0
-                        # Revert back to the old constraint
                         self.constraints = old_constraints
 
-                    # We have exhausted the candidates for this cell
-                    # There is no way to solve the puzzle with the current setting
-                    # We need to go back to the previous step
+                    return False;
+
+        return True;
+
+
+    def csp_3(self):
+        self.constraints = self.get_constraints();
+        self.csp3_helper()
+
+
+    def csp3_helper(self):
+        """
+        Approach 3:
+        	- Fill the empty cell row by row
+        	- Try out the valid candidates in ascending order
+        	- Perform forward checking, stop early when necessary
+        """
+        for x in range(len(self.ans)):
+            for y in range(len(self.ans[0])):
+                if (self.ans[x][y] == 0):
+                    location = (x, y)
+                    candidates = self.constraints.get(location)
+
+                    for c in candidates:
+                        self.ans[x][y] = c
+
+                        # FOR DEBUGGING ONLY
+                        # SHOULD BE REMOVED IN SUBMISSION!
+                        self.print_table()
+
+                        old_constraints = copy.deepcopy(self.constraints)
+                        # Update the constraints with newly assigned value
+                        # Perform forward checking
+                        self.constraints, isAssignable = self.get_constraints_with_early_stop()
+
+                        # Proceed to the next empty cell if passed forward checking
+                        if (isAssignable and self.csp3_helper()):
+                            return True;
+
+                        self.ans[x][y] = 0
+                        self.constraints = old_constraints
+
                     return False;
         
-        # We have solved the puzzle with every cell filled with valid value
         return True;
+
+
+    def csp_4(self):
+        candidate_queue = self.most_constrained()
+        self.csp4_helper(candidate_queue)
+
+
+    def csp4_helper(self, candidate_queue):
+        """
+        Approach 4:
+        	- Fill the empty cell from the most constrained row/column
+        	- Try out the valid candidates in ascending order
+        	- Perform forward checking, stop early when necessary
+        """
+        if (len(candidate_queue) != 0):
+        	cellToFill = pq.heappop(candidate_queue)
+        else:
+        	return True
+
+        # Extract information from the most constrained empty cell
+        location = cellToFill[1]
+        x = location[0]
+        y = location[1]
+        candidates = cellToFill[2]
+
+        for c in candidates:
+            self.ans[x][y] = c
+
+            # FOR DEBUGGING ONLY
+            # SHOULD BE REMOVED IN SUBMISSION!
+            self.print_table()
+
+            # Update the constraint queue & forward check
+            new_candidate_queue, isAssignable = self.most_constrained_with_early_stop()
+
+            if (isAssignable and self.csp4_helper(new_candidate_queue)):
+                return True;
+
+            self.ans[x][y] = 0
+
+        return False;
+
+
+    def get_constraints(self):
+        """
+        Input: 
+        	1. the current Sudoku table (self.ans)
+
+        Output: 
+        	1. the dictionary with key as the coordinates of all empty cells on the table
+               and value is the list of valid numbers that can be assigned to this cell
+
+        This function finds the valid numbers to fill in each empty cell on the current Sudoku board setting.
+        """
+        constraints = {}
+
+        for i in range(len(self.ans)):
+            for j in range(len(self.ans[0])):
+                if (self.ans[i][j] == 0):
+                    location = (i, j)
+                    constraint_list = self.identify_candidates(i, j);
+                    constraints[location] = constraint_list
+
+        return constraints
+
+
+    def get_constraints_with_early_stop(self):
+        """
+        Input: 
+        	1. the current Sudoku table (self.ans)
+
+        Output: 
+        	1. the dictionary with key as the coordinates of all empty cells on the table 
+        	   and value is the list of valid numbers that can be assigned to this cell
+        	2. the result of forward checking
+
+        This function finds the valid numbers to fill in each empty cell on the current Sudoku board setting.
+        At the same time, it forward checks whether the constraints are satisfiable.
+        """
+        constraints = {}
+        isAssignable = True
+
+        for i in range(len(self.ans)):
+            for j in range(len(self.ans[0])):
+                if (self.ans[i][j] == 0):
+                    location = (i, j)
+                    constraint_list = self.identify_candidates(i, j);
+                    if (len(constraint_list) == 0):
+                    	isAssignable = False
+                    constraints[location] = constraint_list
+
+        return constraints, isAssignable
+
+
+    def most_constrained(self):
+        """
+        Input: 
+        	1. the current Sudoku table (self.ans)
+
+        Output: 
+        	1. the priority queue with information about empty cell (coordinates, valid numbers to fill in)
+
+        This function sorts the order to fill the empty cell based on their degree of constraints.
+        """
+        constraints = []
+
+        for i in range(len(self.ans)):
+            for j in range(len(self.ans[0])):
+                if (self.ans[i][j] == 0):
+                    location = (i, j)
+                    constraint_list = self.identify_candidates(i, j);
+                    num = len(constraint_list)
+                    if (num != 0):
+                    	pq.heappush(constraints, (num, location, constraint_list))
+
+        return constraints
+
+
+    def most_constrained_with_early_stop(self):
+        """
+        Input: 
+        	1. the current Sudoku table (self.ans)
+
+        Output: 
+        	1. the priority queue with information about empty cell (coordinates, valid numbers to fill in)
+        	2. the result of forward checking
+
+        This function sorts the order to fill the empty cell based on their degree of constraints.
+        At the same time, it forward checks whether the constraints are satisfiable.
+        """
+        constraints = []
+        isAssignable = True
+
+        for i in range(len(self.ans)):
+            for j in range(len(self.ans[0])):
+                if (self.ans[i][j] == 0):
+                    location = (i, j)
+                    constraint_list = self.identify_candidates(i, j);
+                    num = len(constraint_list)
+                    if (num == 0):
+                    	isAssignable = False
+                    else:
+                    	pq.heappush(constraints, (num, location, constraint_list))
+
+        return constraints, isAssignable
 
 
     def identify_candidates(self, i, j):
         """
-        Input: coordinates of the empty cell in check
-        Output: list of valid values that can be assigned to the given empty cell
+        Input: 
+        	1. coordinates of the empty cell in check
+
+        Output: 
+        	1. list of valid values that can be assigned to the given empty cell
 
         This function returns a list of valid values based on the row, column
         and 3*3 square unique number constraints.
@@ -170,28 +354,9 @@ class Sudoku(object):
         return candidate_list
 
 
-    def get_constraints(self):
-        """
-        Input: the current Sudoku table (self.ans)
-        Output: the dictionary with key as the coordinates of all empty cells on the table
-            and value is the list of valid numbers that can be assigned to this cell
-
-        This function finds the valid numbers for each empty cell on the current Sudoku board setting.
-        """
-        constraints = {}
-
-        for i in range(len(self.ans)):
-            for j in range(len(self.ans[0])):
-                if (self.ans[i][j] == 0):
-                    location = (i, j)
-                    constraint_list = self.identify_candidates(i, j);
-                    constraints[location] = constraint_list
-
-        return constraints
-
-
     def print_table(self):
         # Utility function to print the current Sudoku table
+
         print("----------------------")
         for row in self.ans:
             for j in row:
